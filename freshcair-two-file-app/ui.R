2e9c9029b7d7603929b611b1b180ddadefcd39eb
mariamkg00 <mariamkhadijah00@gmail.com>
2024-04-12 14:16:53 -0700

    organizing file structure

--- freshcair-two-file-app/ui.R
+++ freshcair-two-file-app/ui.R
@@ -52,8 +52,8 @@ body <- dashboardBody(
             "This shiny app was built, in part, to provide Californians a way of visualizing the social and environmental impacts of Senate Bill 1137. California has an ambitious goal to achieve carbon neutrality by 2045, yet it persists in the production of oil and gas, driven by its historical legacy as a significant petroleum producer. Senate Bill (SB) 1137 is a key piece of legislation that aims to regulate oil and gas infrastructure and reduce emissions. A provision in the law mandates a 3,200 foot minimum distance for new oil and gas structures from sensitive areas such as residential areas, schools, and healthcare facilities. The law was signed, but the oil industry successfully lobbied to move it to a referendum vote in November 2024. Now, the fate of SB 1137 depends on public approval, but there is a lack of data-driven analysis of the impacts of the proposed rule to inform the vote."
             
             ),
-            ),
-    ) # end second
+            )
+    ), # end second
     tabItem(tabName = 'oil well explorer',
             
             fluidRow(
